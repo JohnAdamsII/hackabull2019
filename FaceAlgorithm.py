e4eb177f1b087c493f6247826bf2a945a6dd674b
Howard Cheung <howardcheung@USF-Gold-Wifi-226-50-6.laptops.usf.edu>
2019-03-23 18:50:28 -0400

    Work in progress

---
+++ FaceAlgorithm.py
@@ -0,0 +1,64 @@
+
+import cognitive_face as CF
+import requests
+from io import BytesIO
+from PIL import Image, ImageDraw
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+KEY = '3f5f46dbba524e8c9ad99d3a5a63e8c7'  # Replace with a valid subscription key (keeping the quotes in place).
+CF.Key.set(KEY)
+
+BASE_URL = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
+CF.BaseUrl.set(BASE_URL)
+
+def getFaceId(img_url):
+
+    # You can use this example JPG or replace the URL below with your own URL to a JPEG image.
+    #img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+    #img_url = 'https://www.billboard.com/files/media/Danielle-Bregoli-aug-2017-billboard-1548.jpg'
+    # img_url = input()
+    faces = CF.face.detect(img_url)
+    return(faces[0]['faceId'])
+
+
+########### Verify #############
+def getIdentical(input1, input2):
+    headers = {
+        # Request headers
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': '3f5f46dbba524e8c9ad99d3a5a63e8c7',
+    }
+
+    params = urllib.parse.urlencode({
+    })
+
+    faceId1 = getFaceId( input1 )
+    faceId2 = getFaceId( input2 )
+    body = "{ 'faceId1': '%s', 'faceId2': '%s' }" %(faceId1, faceId2)
+    #body = "{ 'faceId1': '96e5e68a-9b60-4b02-8ed6-b796e45cd21d', 'faceId2': '6a436021-2acc-4166-bb6f-09cc1058e6c9' }"
+    #body = str({"faceId": "96e5e68a-9b60-4b02-8ed6-b796e45cd21d", "largeFaceListId": "sample_list", "maxNumOf CandidatesReturned": 10, "mode": "matchPerson"})
+    try:
+        conn = http.client.HTTPSConnection('eastus.api.cognitive.microsoft.com')
+        conn.request("POST", "/face/v1.0/verify%s" % params, body, headers)
+        response = conn.getresponse()
+        data = response.read()
+        print(data)
+        conn.close()
+    except Exception as e:
+        print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+########### Verify #############
+
+with open('links.txt') as f:
+    lines = f.read().splitlines()
+
+print(lines)
+if ( len(lines) % 2 == 1 ):
+    print("Odd number of URLs")
+    exit()
+
+
+for i in range( len(lines) ):
+    url1 = lines[i]
+    getIdentical(lines[i], lines[i])
+    print (x)
--- FaceVerify.py
+++ FaceVerify.py
@@ -16,7 +16,7 @@ params = urllib.parse.urlencode({
 #url1 = input()
 #url2 = input()
 #body = "{ 'url': 'https://i.pinimg.com/originals/b2/cc/74/b2cc74eb9def94c32fdf51beb33d1869.jpg' }"
-body = "{ 'faceId1': '96e5e68a-9b60-4b02-8ed6-b796e45cd21d', 'faceId2': 'db64a557-01f1-4d0c-8840-94baaa3d06b0' }"
+body = "{ 'faceId1': '96e5e68a-9b60-4b02-8ed6-b796e45cd21d', 'faceId2': '6a436021-2acc-4166-bb6f-09cc1058e6c9' }"
 #body = str({"faceId": "96e5e68a-9b60-4b02-8ed6-b796e45cd21d", "largeFaceListId": "sample_list", "maxNumOf CandidatesReturned": 10, "mode": "matchPerson"})
 try:
     conn = http.client.HTTPSConnection('eastus.api.cognitive.microsoft.com')

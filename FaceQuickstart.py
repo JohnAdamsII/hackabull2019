24ef15a113b74eff1cd52d5cd05ffa878175405a
Howard Cheung <howardcheung@USF-Gold-Wifi-226-50-6.laptops.usf.edu>
2019-03-23 17:00:59 -0400

    Got Verify working

--- FaceQuickstart.py
+++ FaceQuickstart.py
@@ -1,12 +1,38 @@
 import cognitive_face as CF
+import requests
+from io import BytesIO
+from PIL import Image, ImageDraw
 
-KEY = '<Subscription Key>'  # Replace with a valid subscription key (keeping the quotes in place).
+KEY = '3f5f46dbba524e8c9ad99d3a5a63e8c7'  # Replace with a valid subscription key (keeping the quotes in place).
 CF.Key.set(KEY)
 
-BASE_URL = 'https://westus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
+BASE_URL = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
 CF.BaseUrl.set(BASE_URL)
 
 # You can use this example JPG or replace the URL below with your own URL to a JPEG image.
-img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+#img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+#img_url = 'https://www.billboard.com/files/media/Danielle-Bregoli-aug-2017-billboard-1548.jpg'
+img_url = input()
 faces = CF.face.detect(img_url)
 print(faces)
+
+#Convert width height to a point in a rectangle
+def getRectangle(faceDictionary):
+    rect = faceDictionary['faceRectangle']
+    left = rect['left']
+    top = rect['top']
+    bottom = left + rect['height']
+    right = top + rect['width']
+    return ((left, top), (bottom, right))
+
+#Download the image from the url
+response = requests.get(img_url)
+img = Image.open(BytesIO(response.content))
+
+#For each face returned use the face rectangle and draw a red box.
+draw = ImageDraw.Draw(img)
+for face in faces:
+    draw.rectangle(getRectangle(face), outline='red')
+
+#Display the image in the users default image browser.
+img.show()
---
+++ FaceVerify.py
@@ -0,0 +1,33 @@
+########### Python 3.2 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64
+
+headers = {
+    # Request headers
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': '3f5f46dbba524e8c9ad99d3a5a63e8c7',
+}
+
+params = urllib.parse.urlencode({
+    # "returnFaceId": 'true',
+    # "returnFaceLandmarks": 'false',
+    # "returnFaceAttributes": 'age',
+})
+
+#url1 = input()
+#url2 = input()
+#body = "{ 'url': 'https://i.pinimg.com/originals/b2/cc/74/b2cc74eb9def94c32fdf51beb33d1869.jpg' }"
+body = "{ 'faceId1': '96e5e68a-9b60-4b02-8ed6-b796e45cd21d', 'faceId2': 'db64a557-01f1-4d0c-8840-94baaa3d06b0' }"
+#body = str({"faceId": "96e5e68a-9b60-4b02-8ed6-b796e45cd21d", "largeFaceListId": "sample_list", "maxNumOf CandidatesReturned": 10, "mode": "matchPerson"})
+try:
+    conn = http.client.HTTPSConnection('eastus.api.cognitive.microsoft.com')
+    conn.request("POST", "/face/v1.0/verify%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+####################################
+
+# {"faceIds": "'96e5e68a-9b60-4b02-8ed6-b796e45cd21d', 'db64a557-01f1-4d0c-8840-94baaa3d06b0'"}
